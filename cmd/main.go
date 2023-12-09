19fabdd4a621ad74d6147e9c9f2048095ffce369
Shirshendu Bhowmick <12836504+shirshendubhowmick@users.noreply.github.com>
2023-12-09 14:56:53 +0530

    added env config

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"main/configs"
 	"main/internal/app"
 	"main/internal/app/router"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		panic(err)
+	}
+	configs.InitEnvConfig()
+
 	appInstance := app.Init()
 
 	router.Init(appInstance)
